86c14328bc6ea08a25e3880b50fd43c2290f17b3
sukruburakcetin <cetin.sukruburak@gmail.com>
2023-12-12 16:28:20 +0300

    files are customized for better clarity and readability

--- haversine-with-rapids.py
+++ haversine-with-rapids.py
@@ -1,40 +1,62 @@
+import math
 import time
 import warnings
 
 import cudf
 import cuspatial
-import geopandas as gpd
 import pandas as pd
 
 warnings.filterwarnings("ignore")
 
-# Load the DataFrames
-myLocation = gpd.read_file(r"static/data/main/startLocation.shp")
-worldCities = pd.read_csv(r"static/data/main/worldCities.csv")
+# Load worldCities Data
+worldCities = pd.read_csv(r"static/data/in-main/worldCities.csv")
 
-# Convert Pandas DataFrame to
-worldCities = cudf.from_pandas(worldCities)
-start = time.time()
+# Extract main location(suleymaniye kulliyesi) lat and lng => from
+lon1 = 28.965305
+lat1 = 41.015177
 
-# Create cuSpatial GeoSeries from cuDF Dataframe
-cuGeoSeries = cuspatial.GeoSeries.from_points_xy(worldCities[['lat', 'lng']].interleave_columns())
+# Create column names, assign preloading values
+worldCities["merged_travel_time"] = ""
+worldCities["merged_distance"] = ""
+worldCities["icon"] = "heart"
 
 # Assign start location to the current DataFrame rows
-worldCities["myLocation_lat"] = myLocation["lat"][0]
-worldCities["myLocation_lng"] = myLocation["lng"][0]
+worldCities["SULEYMANIYE_KULLIYESI_LAT"] = lat1
+worldCities["SULEYMANIYE_KULLIYESI_LON"] = lon1
 
-# Create cuSpatial GeoSeries from cuDF Dataframe
-atlGeoSeries = cuspatial.GeoSeries.from_points_xy(worldCities[['myLocation_lat', 'myLocation_lng']].interleave_columns())
+# Convert Pandas DataFrame to cuda dataFrame
+worldCities = cudf.from_pandas(worldCities)
+
+# Create cuSpatial GeoSeries for WorldCities Data from cuDF Dataframe
+cuGeoSeries = cuspatial.GeoSeries.from_points_xy(worldCities[['lat', 'lng']].interleave_columns())
+
+# Create cuSpatial GeoSeries for Input Data(Süleymaniye Külliyesi) from cuDF Dataframe
+atlGeoSeries = cuspatial.GeoSeries.from_points_xy(
+    worldCities[['SULEYMANIYE_KULLIYESI_LAT', 'SULEYMANIYE_KULLIYESI_LON']].interleave_columns())
+
+# Start timer
+start = time.time()
 
 # Calculate Haversine Distance of cuDF dataframe to comparator point
 worldCities['distance'] = cuspatial.haversine_distance(cuGeoSeries, atlGeoSeries)
+
+# Stop timer
 end = time.time()
 print("python computing distance time in ms={}".format((end - start) * 1000))
 
 # Calculate travel time
 worldCities["travel_time"] = worldCities["distance"] / 820
 
-print(worldCities)
+# Split travel time into hours and minutes
+worldCities['hours'] = worldCities['travel_time'].apply(lambda x: int(math.floor(x)))
+worldCities['minutes'] = round((worldCities['travel_time'] - worldCities['hours']) * 60)
+
+# Convert column names with meaningful tags in order to improve readability in Turkish
+worldCities['merged_travel_time'] = worldCities["hours"].astype(str) + " saat" + worldCities['minutes'].astype(str) + " dakika"
+worldCities['merged_distance'] = worldCities["distance"].astype(str) + " km"
+
+# Rename column names to translate some of the columns into Turkish
+worldCities.rename(columns={"city": "SEHIR", "country": "ULKE", "capital": "BASKENT", "population": "NUFUS", "merged_travel_time": "SEYAHAT_SURESI", "merged_distance": "MESAFE"}, inplace=True)
 
 # Write current DataFrame to local as csv
-worldCities.to_csv("worldCities_with_rapids.csv")
+worldCities.to_csv("./static/data/out-haversine-with-rapids/with_rapids_tr_worldCities.csv")
--- haversine-without-rapids.py
+++ haversine-without-rapids.py
@@ -1,8 +1,8 @@
-import geopandas as gpd
-import pandas as pd
 import math
 import time
 import warnings
+
+import pandas as pd
 
 warnings.filterwarnings("ignore")
 
@@ -26,19 +26,26 @@ def haversine(lat1, lon1, lat2, lon2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     distance = earth_radius * c
 
-    return distance
+    return round(distance)
 
 
-# Load the DataFrames
-myLocation = gpd.read_file(r"static/data/main/startLocation.shp")
-worldCities = pd.read_csv(r"static/data/main/worldCities.csv")
+# Load the worldCities data to the DataFrame
+worldCities = pd.read_csv(r"static/data/in-main/worldCities.csv")
+
+worldCities["merged_travel_time"] = ""
+worldCities["merged_distance"] = ""
+worldCities["icon"] = "heart"
+
+# Extract main location(suleymaniye kulliyesi) lat and lng => from
+lon1 = 28.965305
+lat1 = 41.015177
+
+# Assign start location to the current DataFrame rows
+worldCities["SULEYMANIYE_KULLIYESI_LAT"] = lat1
+worldCities["SULEYMANIYE_KULLIYESI_LON"] = lon1
 
 # Start timer
 start = time.time()
-
-# Extract main location lat and lng => from
-lon1 = myLocation['lng'][0]
-lat1 = myLocation['lat'][0]
 
 # Alternative for loop method (0)
 # for j in range(len(worldCities)):
@@ -60,7 +67,19 @@ print("python computing distance time in ms={}".format((end - start) * 1000))
 # Calculate travel time
 worldCities["travel_time"] = worldCities["distance"] / 820
 
-print(worldCities)
+# Split travel time into hours and minutes
+worldCities['hours'] = worldCities['travel_time'].apply(lambda x: int(math.floor(x)))
+worldCities['minutes'] = round((worldCities['travel_time'] - worldCities['hours']) * 60)
+worldCities['minutes'] = worldCities['minutes'].astype(int)
+
+# Convert column names with meaningful tags in order to improve readability in Turkish (saat = hour ; dakika = minute)
+worldCities['merged_travel_time'] = worldCities["hours"].astype(str) + " saat " + worldCities['minutes'].astype(
+    str) + " dakika"
+worldCities['merged_distance'] = worldCities["distance"].astype(str) + " km"
+
+# Rename column names to translate some of the columns into Turkish
+worldCities.rename(columns={"city": "SEHIR", "country": "ULKE", "capital": "BASKENT", "population": "NUFUS",
+                            "merged_travel_time": "SEYAHAT_SURESI", "merged_distance": "MESAFE"}, inplace=True)
 
 # Write current DataFrame to local as csv
-worldCities.to_csv("worldCities_without_rapids.csv")
+worldCities.to_csv("./static/data/out-haversine-without-rapids/without_rapids_tr_worldCities.csv")
